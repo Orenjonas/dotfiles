76b7fb70765f534a1b0a3b8107425f76ac6f8266
Jonas Oren <jonas.oren@gmail.com>
2023-02-24 08:54:37 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Vim keybindings in the terminal
-fish_vi_key_bindings
+## Vim keybindings in the terminal
+#fish_vi_key_bindings
