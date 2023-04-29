8f112569228e5bd739cd8271134ba1b2814cb9e4
Jonas Oren <jonas.oren@gmail.com>
2023-04-29 13:29:56 +0200

    Enable nertw and revert J to normal

--- nvim/lua/user/keybindings.lua
+++ nvim/lua/user/keybindings.lua
@@ -6,8 +6,8 @@ vim.keymap.set("t", "jk", [[<C-\><C-n>]], { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- append line below to current line without moving cursor
-vim.keymap.set("n", "J", "mzJ`z")
+-- -- append line below to current line without moving cursor
+-- vim.keymap.set("n", "J", "mzJ`z")
 
 -- yank to system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
@@ -19,12 +19,12 @@ vim.keymap.set("n", "<leader>erc", ":edit ~/.config/nvim/init.lua <cr>")
 -- edit dotfiles
 vim.keymap.set("n", "<leader>edf", ":edit ~/.dotfiles <cr>")
 
--- open urls with netrw disabled (when using nvim-tree)
-if vim.fn.has("mac") == 1 then
-    vim.keymap.set("n", "gx", '<Cmd>call jobstart(["open", expand("<cfile>")], {"detach": v:true})<CR>')
-elseif vim.fn.has("unix") == 1 then
-    vim.keymap.set("n", "gx", '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>', {})
-end
+-- -- open urls with netrw disabled (when using nvim-tree)
+-- if vim.fn.has("mac") == 1 then
+--     vim.keymap.set("n", "gx", '<Cmd>call jobstart(["open", expand("<cfile>")], {"detach": v:true})<CR>')
+-- elseif vim.fn.has("unix") == 1 then
+--     vim.keymap.set("n", "gx", '<Cmd>call jobstart(["xdg-open", expand("<cfile>")], {"detach": v:true})<CR>', {})
+-- end
 
 -- -- Splits
 -- vim.keymap.set( "n", "<leader>;", "<cmd>vsplit n<CR>" )
