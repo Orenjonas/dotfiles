c62db26dbd2c7653c97d30f9049adf67c05e8252
Jonas Oren <jonas.oren@gmail.com>
2023-06-02 07:07:45 +0200

    Add placeholders to neogen

--- nvim/after/plugin/neogen.lua
+++ nvim/after/plugin/neogen.lua
@@ -1,4 +1,12 @@
 require('neogen').generate()
 
 local opts = { noremap = true, silent = true }
+
+require('neogen').setup {
+    enabled = true,             --if you want to disable Neogen
+    input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
+    enable_placeholders = true,
+    -- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
+}
+
 vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", opts)
