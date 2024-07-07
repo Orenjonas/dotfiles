910b1403ec3fa4c76b76cf0332ad6adc6cff7eb3
Jonas Øren <jonas.oren@gmail.com>
2024-07-07 09:50:27 +0200

    bugfix

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -119,12 +119,6 @@ require('luasnip.loaders.from_vscode').lazy_load()
 
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.omnifunc.setup({
-    tabcomplete = true,
-    use_fallback = true,
-    update_on_delete = true,
-})
-
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
